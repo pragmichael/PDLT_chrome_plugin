98ddec6f556e2cbeb1356db588310373dbed5782
pragmichael <michael.popchuk@pragmaticdlt.com>
2022-06-09 18:37:03 +0300

    Button listener added and all info collected

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 var template_request_body = `
+    <script type="text/javascript">
+	  function send_template_data () {
+			var select = document.getElementById('templates');
+			var template = select.options[select.selectedIndex].value;
+			
+			console.log(template); 
+		}
+	</script>
+
     <div data-v-1c326b38="" class="up-alert up-alert-success">
         <div class="up-alert-icon">
             <div class="up-icon sm"></div>
@@ -23,58 +32,69 @@ var template_request_body = `
                 </p>
             </div>
         </div> 
-            <button type="button" class="up-btn-reset up-alert-close" onclick="send_template_data ()">
+
+            <button type="button" id="send_template_data" class="up-btn-reset up-alert-close">
                 <span class="sr-only">Close the <!----> alert</span> 
                 <div class="up-icon sm">
                 </div>
             </button>
     </div> 
-    <script async src="https://pragmaticdlt.com/js/plugin_script.js"></script>
 `;
 
-function write_log (otpt) {
-	console.log("write log start")
-	const log_line = document.createElement("li");
-	log_line.className="list-group-item"
-	log_line.innerHTML = otpt;
-	console.log(log_line)
+function input_template () {
+	let alert_area = document.querySelector(".alerts");
+	let popup = document.createElement("div");
+		popup.className="up-alert-content break";
+		popup.innerHTML = template_request_body;
+//	console.log (alert_area);
+	alert_area.appendChild(popup);
 
-//	output the result
-//	let pdlt_log = document.getElementsByClassName("pdlt_log");
-//	pdlt_log[0].appendChild(log_line);
-
-	console.log("write log finish");
+	listen_button();
 }
 
+function listen_button () { 
+    var link = document.getElementById('send_template_data');
+    console.log ("button retrieved")
+    link.addEventListener('click', function() {
+        console.log("listener shoot");
 
+        send_template_data ();
+    });
+};
 
+let payment_method;
+let budget;
+let connects;
+let proposal_check;
+let sales_person;
+let proposal_id;
+let date_posted;
 
 function wait_proposal () {
 	console.log('wait_proposal start');
-	var proposal_check = document.querySelector(".m-0").innerText;
+	proposal_check = document.querySelector(".m-0").innerText;
 	if (proposal_check.includes("submitted") != null) {
 		let url = document.URL;
-			var proposal_id = url.substring(
+			proposal_id = url.substring(
 				url.lastIndexOf('/') + 1,
 				url.indexOf("?"), 
 				);
 
-		var sales_person = document.querySelector(".up-fe-proposal-brief-info a").innerText;
-		var payment_method = "N/A";
-			if (document.querySelector("#payment-method-not-verified-trigger") == null) {
-				payment_method = document.querySelector("#fe-payment-verified-text").innerText; }
-				else {
-					payment_method = document.querySelector("#payment-method-not-verified-trigger").innerText;
-				};
-		var date_posted = document.querySelector(".list-inline.mb-10 span").innerText;
+		if (document.querySelector(".up-fe-proposal-brief-info a")) {sales_person = document.querySelector(".up-fe-proposal-brief-info a").innerText;}
+		if (document.querySelector("#payment-method-not-verified-trigger") == null) {
+			payment_method = document.querySelector("#fe-payment-verified-text").innerText; }
+			else {
+				payment_method = document.querySelector("#payment-method-not-verified-trigger").innerText;
+			};
+		const date_posted = document.querySelector(".list-inline.mb-10 span").innerText;
 		if (document.querySelector(".m-0-bottom strong")) {
-			var connects = document.querySelector(".m-0-bottom strong").innerText;
+			connects = document.querySelector(".m-0-bottom strong").innerText;
 		} 
 		if (document.querySelector(".pull-right.m-0.d-none.d-md-block.text-right span") == null) {
-			var budget = "hourly";
+			budget = "hourly";
 		}
 			else {
-				var budget = document.querySelector(".pull-right.m-0.d-none.d-md-block.text-right span").innerText;
+				budget = document.querySelector(".pull-right.m-0.d-none.d-md-block.text-right span").innerText;
 			};
 
 	console.log(proposal_check);
@@ -93,20 +113,22 @@ function wait_proposal () {
 
 setTimeout(wait_proposal, 3000);
 
-function input_template () {
-	let alert_area = document.querySelector(".alerts");
-	let popup = document.createElement("div");
-		popup.className="up-alert-content break";
-		popup.innerHTML = template_request_body;
-	console.log (alert_area);
-	alert_area.appendChild(popup);
-}
 
 function send_template_data () {
+	document.querySelector('#send_template_data').addEventListener("click", function () {
+       console.log('Send_template_data activated');
+    });
 	var select = document.getElementById('templates');
 	var template = select.options[select.selectedIndex].value;
 	
-	console.log(value); 
+	console.log(template);
+	console.log(proposal_check);
+	console.log(proposal_id);
+	console.log(sales_person); 
+	console.log(payment_method);
+	console.log(date_posted);
+	console.log(connects);
+	console.log(budget);
 }
 
 
